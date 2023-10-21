9fd2de44bc671a2dde1c55841c6c3bf2afe03966
Д. М <skider96@abv.bg>
2023-10-21 16:48:31 +0300

    Add project files.

---
+++ 2.Zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2.Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
